4e3c735c0c48dfeace4ddace0b455835cffdcf68
Yves Vanduynslager <yves.vanduynslager@telenet.be>
2017-09-07 16:54:36 +0200

    removed comments

--- src/Darts/Data/DartsDataInitializer.cs
+++ src/Darts/Data/DartsDataInitializer.cs
@@ -35,33 +35,6 @@ namespace Darts.Data
                 await _userManager.CreateAsync(user, "Darts123");
                 await _userManager.AddClaimAsync(user, new Claim(ClaimTypes.Role, "speler"));
 
-                //SpelerWedstrijden adden aan context, met speler en wedstrijd
-                //SpelerWedstrijd(speler 1 dan 2, zelfde wedstrijd)
-                //var speler1 = new Speler("jan.deman@telenet.be", "Jan", "De man");
-                //var speler2 = new Speler("dartsmaster@telenet.be", "Bert", "Vanachterdehoek");
-                //var speler3 = new Speler("reetman@telenet.be", "Jantje", "Smit");
-
-                //Wedstrijd w = new Wedstrijd(DateTime.Now);
-                //SpelerWedstrijd sw1 = new SpelerWedstrijd(speler1, w, 3, speler2.Voornaam + " " + speler2.Naam);
-                //SpelerWedstrijd sw2 = new SpelerWedstrijd(speler2, w, 0, speler1.Voornaam + " " + speler1.Naam);
-
-                //w = new Wedstrijd(new DateTime(2010,1,12));
-                //SpelerWedstrijd sw3 = new SpelerWedstrijd(speler2, w, 2, speler3.Voornaam + " " + speler3.Naam);
-                //SpelerWedstrijd sw4 = new SpelerWedstrijd(speler3, w, 1, speler2.Voornaam + " " + speler2.Naam);
-
-                //SpelerWedstrijd[] sws = { sw1, sw2, sw3, sw4 };
-                //_dbContext.SpelerWedstrijden.AddRange(sws);
-                //_dbContext.SaveChanges();
-
-                //speler1.AddWedstrijd(new Wedstrijd(DateTime.Now, speler1, speler2, 3));
-                //speler2.AddWedstrijd(new Wedstrijd(DateTime.Now, speler2, speler1, 1));
-                //speler3.AddWedstrijd(new Wedstrijd(DateTime.Now, speler3, speler2, 1));
-
-                //Speler[] spelers = { speler1, speler2, speler3 };
-
-                //_dbContext.Spelers.AddRange(spelers);
-                //_dbContext.SaveChanges();
-
                 //await InitializeUsersAndCustomers();
             }
         }
